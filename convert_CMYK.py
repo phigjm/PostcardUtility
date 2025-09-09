4b6a0b39acc8310f34d36374a5aa60d157247082
gjm <gjm>
2025-09-09 03:09:03 +0200

    add option to convert pdf to CMYK

---
+++ convert_CMYK.py
@@ -0,0 +1,68 @@
+import subprocess
+import os
+
+
+args_hq = [
+    "-sDEVICE=pdfwrite",
+    "-dUseBleedBox",
+    "-dQUIET",
+    "-dPDFSETTINGS=/prepress",  # Higher quality setting
+    "-dDetectDuplicateImages",
+    "-dAutoFilterColorImages=false",
+    "-dAutoFilterGrayImages=false",
+    "-dColorImageFilter=/FlateEncode",
+    "-dGrayImageFilter=/FlateEncode",
+    "-dDownsampleMonoImages=false",
+    "-dDownsampleGrayImages=false",
+    "-dColorImageResolution=300",
+    "-dGrayImageResolution=300",
+    "-dCompressPages=true",
+    "-dSubsetFonts=true",
+    "-sProcessColorModel=DeviceCMYK",
+    "-sColorConversionStrategy=CMYK",
+    "-sColorConversionStrategyForImages=CMYK",
+    "-dCompatibilityLevel=1.4",
+    "-dNOPAUSE",
+    "-dBATCH",
+]
+
+args_minimum = [
+    "-sDEVICE=pdfwrite",
+    "-dColorConversionStrategy=/CMYK",
+    "-dProcessColorModel=/DeviceCMYK",
+    "-sColorConversionStrategyForImages=CMYK",
+    "-dCompatibilityLevel=1.4",
+    "-dNOPAUSE",
+    "-dBATCH",
+]
+
+
+def convertPDFtoCMYK(pdf_in, pdf_out=None):
+    # convert pdf_in to absolute path
+    pdf_in = os.path.abspath(pdf_in)
+
+    if pdf_out is None:
+        pdf_out = os.path.splitext(pdf_in)[0] + "_CMYK.pdf"
+    gs_path = "gswin64c.exe"  # Adjust to your GhostScript executable path if needed
+
+    quality_args = args_hq
+
+    args = [
+        gs_path,
+        "-o",
+        pdf_out,
+        *quality_args,  # Use high quality settings
+        r"-f",
+        pdf_in,  # Input file prefixed with -f
+    ]
+
+    # print the command for debugging
+    print("Running command:", " ".join(args))
+
+    subprocess.run(args, check=True)
+    return pdf_out
+
+
+if __name__ == "__main__":
+    input_pdf = "Examples/postcard.pdf"
+    convertPDFtoCMYK(input_pdf)
